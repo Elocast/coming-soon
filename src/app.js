9ecc53290e0c8815b8e88eaab365ee34abc1571e
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2018-04-10 00:26:47 +0100

    Wrap app.js in 'DOMContentLoaded' event

--- src/app.js
+++ src/app.js
@@ -1,31 +1,32 @@
 import Game from './Game'
 import './utils'
 
-const canvas = document.querySelector('canvas')
+document.addEventListener('DOMContentLoaded', function() {
+  const canvas = document.querySelector('canvas')
+  const game = new Game(canvas)
+  game.start()
 
-const game = new Game(canvas)
-game.start()
+  const mainLoop = (function() {
+    const skipTicks = 1000 / game.fps
+    let loops = 0
+    let maxFrameSkip = 10
+    let nextGameTick = (new Date()).getTime()
 
-const mainLoop = (function() {
-  const skipTicks = 1000 / game.fps
-  let loops = 0
-  let maxFrameSkip = 10
-  let nextGameTick = (new Date()).getTime()
+    return function() {
+      loops = 0
 
-  return function() {
-    loops = 0
+      while (!game.paused &&
+        (new Date()).getTime() > nextGameTick &&
+        loops < maxFrameSkip
+      ) {
+        game.update()
+        nextGameTick += skipTicks
+        loops++
+      }
 
-    while (!game.paused &&
-      (new Date()).getTime() > nextGameTick &&
-      loops < maxFrameSkip
-    ) {
-      game.update()
-      nextGameTick += skipTicks
-      loops++
+      game.draw()
     }
+  })()
 
-    game.draw()
-  }
-})()
-
-window.onEveryFrame(mainLoop)
+  window.onEveryFrame(mainLoop)
+})
